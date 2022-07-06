cd4aa7ae33b8100b94f2853aa2dc9117f0b90e2c
timak9 <88079083+timak9@users.noreply.github.com>
2022-07-07 00:54:46 +0300

    a

---
+++ update player wishlist.sql
@@ -0,0 +1,1946 @@
+UPDATE player SET `25` = 9996 WHERE `id` = 1;
+UPDATE player SET `25` = 9997 WHERE `id` = 2;
+UPDATE player SET `25` = 9991 WHERE `id` = 3;
+UPDATE player SET `25` = 10000 WHERE `id` = 4;
+UPDATE player SET `25` = 9994 WHERE `id` = 5;
+
+UPDATE player SET `25` = 9986 WHERE `id` = 7;
+UPDATE player SET `25` = 9987 WHERE `id` = 8;
+UPDATE player SET `25` = 9998 WHERE `id` = 9;
+UPDATE player SET `25` = 9981 WHERE `id` = 10;
+UPDATE player SET `25` = 9999 WHERE `id` = 11;
+
+UPDATE player SET `25` = 9989 WHERE `id` = 13;
+
+
+UPDATE player SET `25` = 9973 WHERE `id` = 16;
+UPDATE player SET `25` = 9995 WHERE `id` = 17;
+
+UPDATE player SET `25` = 9982 WHERE `id` = 19;
+
+
+
+UPDATE player SET `25` = 9981 WHERE `id` = 23;
+
+
+
+UPDATE player SET `25` = 9977 WHERE `id` = 27;
+UPDATE player SET `25` = 9968 WHERE `id` = 28;
+UPDATE player SET `25` = 9978 WHERE `id` = 29;
+
+
+UPDATE player SET `25` = 9972 WHERE `id` = 32;
+
+
+
+
+UPDATE player SET `25` = 9983 WHERE `id` = 37;
+
+
+
+
+
+
+
+UPDATE player SET `25` = 9961 WHERE `id` = 45;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+UPDATE player SET `25` = 9969 WHERE `id` = 60;
+
+
+
+
+UPDATE player SET `25` = 9979 WHERE `id` = 65;
+
+
+
+
+UPDATE player SET `25` = 9976 WHERE `id` = 70;
+
+
+
+
+UPDATE player SET `25` = 9986 WHERE `id` = 75;
+
+
+UPDATE player SET `25` = 9960 WHERE `id` = 78;
+
+
+UPDATE player SET `25` = 9959 WHERE `id` = 81;
+
+
+
+UPDATE player SET `25` = 9973 WHERE `id` = 85;
+
+
+
+
+
+
+
+
+
+
+
+
+
+UPDATE player SET `25` = 9972 WHERE `id` = 99;
+
+
+UPDATE player SET `25` = 9958 WHERE `id` = 102;
+
+
+
+
+
+
+
+UPDATE player SET `25` = 9962 WHERE `id` = 110;
+
+
+UPDATE player SET `25` = 9971 WHERE `id` = 113;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+UPDATE player SET `25` = 9956 WHERE `id` = 134;
+UPDATE player SET `25` = 9955 WHERE `id` = 135;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+UPDATE player SET `25` = 9977 WHERE `id` = 153;
+
+
+
+
+
+
+
+
+
+
+
+
+
+UPDATE player SET `25` = 9975 WHERE `id` = 167;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+UPDATE player SET `25` = 9969 WHERE `id` = 182;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+UPDATE player SET `25` = 9969 WHERE `id` = 243;
+
+
+
+
+
+
+
+
+UPDATE player SET `25` = 9990 WHERE `id` = 252;
+
+
+
+
+
+
+UPDATE player SET `25` = 9966 WHERE `id` = 259;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+UPDATE player SET `25` = 9961 WHERE `id` = 396;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+UPDATE player SET `20` = 9992 WHERE `id` = 1;
+UPDATE player SET `20` = 9997 WHERE `id` = 2;
+UPDATE player SET `20` = 10000 WHERE `id` = 3;
+UPDATE player SET `20` = 9985 WHERE `id` = 4;
+UPDATE player SET `20` = 9991 WHERE `id` = 5;
+UPDATE player SET `20` = 9963 WHERE `id` = 6;
+UPDATE player SET `20` = 9984 WHERE `id` = 7;
+UPDATE player SET `20` = 9999 WHERE `id` = 8;
+UPDATE player SET `20` = 9982 WHERE `id` = 9;
+UPDATE player SET `20` = 9996 WHERE `id` = 10;
+UPDATE player SET `20` = 9998 WHERE `id` = 11;
+
+UPDATE player SET `20` = 9962 WHERE `id` = 13;
+
+
+
+
+UPDATE player SET `20` = 9961 WHERE `id` = 18;
+UPDATE player SET `20` = 9990 WHERE `id` = 19;
+UPDATE player SET `20` = 9950 WHERE `id` = 20;
+UPDATE player SET `20` = 9934 WHERE `id` = 21;
+UPDATE player SET `20` = 9949 WHERE `id` = 22;
+UPDATE player SET `20` = 9989 WHERE `id` = 23;
+
+
+
+UPDATE player SET `20` = 9948 WHERE `id` = 27;
+
+UPDATE player SET `20` = 9960 WHERE `id` = 29;
+
+UPDATE player SET `20` = 9988 WHERE `id` = 31;
+
+UPDATE player SET `20` = 9959 WHERE `id` = 33;
+
+UPDATE player SET `20` = 9987 WHERE `id` = 35;
+
+
+
+
+
+
+
+UPDATE player SET `20` = 9947 WHERE `id` = 43;
+
+UPDATE player SET `20` = 9946 WHERE `id` = 45;
+
+UPDATE player SET `20` = 9945 WHERE `id` = 47;
+UPDATE player SET `20` = 9944 WHERE `id` = 48;
+
+
+
+
+
+
+
+UPDATE player SET `20` = 9943 WHERE `id` = 56;
+
+
+UPDATE player SET `20` = 9952 WHERE `id` = 59;
+
+UPDATE player SET `20` = 9995 WHERE `id` = 61;
+
+UPDATE player SET `20` = 9942 WHERE `id` = 63;
+
+
+UPDATE player SET `20` = 9980 WHERE `id` = 66;
+
+UPDATE player SET `20` = 9979 WHERE `id` = 68;
+UPDATE player SET `20` = 9978 WHERE `id` = 69;
+
+
+
+UPDATE player SET `20` = 9994 WHERE `id` = 73;
+
+UPDATE player SET `20` = 9977 WHERE `id` = 75;
+
+
+UPDATE player SET `20` = 9976 WHERE `id` = 78;
+UPDATE player SET `20` = 9954 WHERE `id` = 79;
+
+
+
+UPDATE player SET `20` = 9955 WHERE `id` = 83;
+
+UPDATE player SET `20` = 9975 WHERE `id` = 85;
+
+
+
+
+
+
+
+UPDATE player SET `20` = 9974 WHERE `id` = 93;
+
+
+
+
+
+UPDATE player SET `20` = 9993 WHERE `id` = 99;
+
+
+UPDATE player SET `20` = 9940 WHERE `id` = 102;
+
+
+
+UPDATE player SET `20` = 9973 WHERE `id` = 106;
+
+
+
+
+
+
+UPDATE player SET `20` = 9956 WHERE `id` = 113;
+
+
+
+
+
+UPDATE player SET `20` = 9939 WHERE `id` = 119;
+
+
+
+UPDATE player SET `20` = 9938 WHERE `id` = 123;
+
+UPDATE player SET `20` = 9972 WHERE `id` = 125;
+UPDATE player SET `20` = 9971 WHERE `id` = 126;
+
+
+
+
+
+
+UPDATE player SET `20` = 9957 WHERE `id` = 133;
+
+UPDATE player SET `20` = 9970 WHERE `id` = 135;
+
+UPDATE player SET `20` = 9937 WHERE `id` = 137;
+UPDATE player SET `20` = 9969 WHERE `id` = 138;
+
+
+
+
+
+
+
+
+UPDATE player SET `20` = 9968 WHERE `id` = 147;
+
+UPDATE player SET `20` = 9936 WHERE `id` = 149;
+
+
+
+UPDATE player SET `20` = 9935 WHERE `id` = 153;
+
+
+
+
+
+
+
+UPDATE player SET `20` = 9934 WHERE `id` = 161;
+
+
+UPDATE player SET `20` = 9967 WHERE `id` = 164;
+
+
+
+
+UPDATE player SET `20` = 9933 WHERE `id` = 169;
+
+
+
+
+UPDATE player SET `20` = 9958 WHERE `id` = 174;
+
+
+
+
+
+
+
+UPDATE player SET `20` = 9966 WHERE `id` = 182;
+
+
+
+
+UPDATE player SET `20` = 9981 WHERE `id` = 187;
+UPDATE player SET `20` = 9965 WHERE `id` = 188;
+
+
+
+
+
+UPDATE player SET `20` = 9964 WHERE `id` = 194;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+UPDATE player SET `20` = 9983 WHERE `id` = 243;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+UPDATE player SET `20` = 9986 WHERE `id` = 371;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+UPDATE player SET `16` = 9992 WHERE `id` = 1;
+UPDATE player SET `16` = 9999 WHERE `id` = 2;
+UPDATE player SET `16` = 10000 WHERE `id` = 4;
+UPDATE player SET `16` = 9993 WHERE `id` = 5;
+UPDATE player SET `16` = 9994 WHERE `id` = 6;
+UPDATE player SET `16` = 9997 WHERE `id` = 9;
+UPDATE player SET `16` = 9988 WHERE `id` = 10;
+UPDATE player SET `16` = 9991 WHERE `id` = 11;
+UPDATE player SET `16` = 9959 WHERE `id` = 12;
+UPDATE player SET `16` = 9998 WHERE `id` = 13;
+UPDATE player SET `16` = 9980 WHERE `id` = 15;
+UPDATE player SET `16` = 9989 WHERE `id` = 17;
+UPDATE player SET `16` = 9990 WHERE `id` = 21;
+UPDATE player SET `16` = 9904 WHERE `id` = 25;
+UPDATE player SET `16` = 9996 WHERE `id` = 27;
+UPDATE player SET `16` = 9981 WHERE `id` = 29;
+UPDATE player SET `16` = 9979 WHERE `id` = 30;
+
+UPDATE player SET `16` = 9982 WHERE `id` = 43;
+UPDATE player SET `16` = 9923 WHERE `id` = 44;
+UPDATE player SET `16` = 9924 WHERE `id` = 49;
+UPDATE player SET `16` = 9995 WHERE `id` = 52;
+UPDATE player SET `16` = 9983 WHERE `id` = 53;
+UPDATE player SET `16` = 9922 WHERE `id` = 56;
+UPDATE player SET `16` = 9921 WHERE `id` = 76;
+UPDATE player SET `16` = 9920 WHERE `id` = 79;
+
+UPDATE player SET `16` = 9919 WHERE `id` = 100;
+UPDATE player SET `16` = 9914 WHERE `id` = 107;
+UPDATE player SET `16` = 9915 WHERE `id` = 130;
+
+UPDATE player SET `16` = 9913 WHERE `id` = 134;
+
+UPDATE player SET `16` = 9911 WHERE `id` = 141;
+UPDATE player SET `16` = 9912 WHERE `id` = 142;
+UPDATE player SET `16` = 9986 WHERE `id` = 153;
+
+UPDATE player SET `16` = 9918 WHERE `id` = 169;
+UPDATE player SET `16` = 9904 WHERE `id` = 170;
+
+
+
+
+UPDATE player SET `16` = 9917 WHERE `id` = 233;
+UPDATE player SET `16` = 9987 WHERE `id` = 252;
+
+
+
+UPDATE player SET `16` = 9985 WHERE `id` = 259;
+
+UPDATE player SET `16` = 9916 WHERE `id` = 265;
+
+
+
+UPDATE player SET `16` = 9984 WHERE `id` = 287;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+UPDATE player SET `16` = 9944 WHERE `id` = 19;
+UPDATE player SET `16` = 9943 WHERE `id` = 26;
+UPDATE player SET `16` = 9942 WHERE `id` = 28;
+UPDATE player SET `16` = 9938 WHERE `id` = 39;
+UPDATE player SET `16` = 9937 WHERE `id` = 51;
+UPDATE player SET `16` = 9936 WHERE `id` = 54;
+
+UPDATE player SET `16` = 9934 WHERE `id` = 58;
+UPDATE player SET `16` = 9935 WHERE `id` = 59;
+UPDATE player SET `16` = 9940 WHERE `id` = 65;
+UPDATE player SET `16` = 9886 WHERE `id` = 66;
+UPDATE player SET `16` = 9934 WHERE `id` = 73;
+UPDATE player SET `16` = 9941 WHERE `id` = 75;
+UPDATE player SET `16` = 9885 WHERE `id` = 82;
+UPDATE player SET `16` = 9884 WHERE `id` = 85;
+UPDATE player SET `16` = 9933 WHERE `id` = 86;
+UPDATE player SET `16` = 9883 WHERE `id` = 93;
+
+UPDATE player SET `16` = 9882 WHERE `id` = 99;
+UPDATE player SET `16` = 9932 WHERE `id` = 105;
+UPDATE player SET `16` = 9931 WHERE `id` = 109;
+UPDATE player SET `16` = 9939 WHERE `id` = 110;
+
+UPDATE player SET `16` = 9930 WHERE `id` = 121;
+UPDATE player SET `16` = 9924 WHERE `id` = 124;
+UPDATE player SET `16` = 9881 WHERE `id` = 126;
+UPDATE player SET `16` = 9880 WHERE `id` = 129;
+UPDATE player SET `16` = 9923 WHERE `id` = 132;
+UPDATE player SET `16` = 9929 WHERE `id` = 138;
+
+UPDATE player SET `16` = 9922 WHERE `id` = 147;
+UPDATE player SET `16` = 9928 WHERE `id` = 149;
+UPDATE player SET `16` = 9921 WHERE `id` = 151;
+UPDATE player SET `16` = 9927 WHERE `id` = 154;
+
+
+
+
+
+
+
+
+
+UPDATE player SET `16` = 9879 WHERE `id` = 184;
+
+UPDATE player SET `16` = 9926 WHERE `id` = 187;
+UPDATE player SET `16` = 9925 WHERE `id` = 188;
+
+
+
+
+
+UPDATE player SET `16` = 9924 WHERE `id` = 194;
+
+
+
+
+
+UPDATE player SET `16` = 9878 WHERE `id` = 209;
+
+
+UPDATE player SET `16` = 9877 WHERE `id` = 217;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+UPDATE player SET `16` = 9876 WHERE `id` = 246;
+
+
+
+
+
+
+
+UPDATE player SET `16` = 9875 WHERE `id` = 264;
+
+UPDATE player SET `16` = 9874 WHERE `id` = 267;
+UPDATE player SET `16` = 9873 WHERE `id` = 275;
+UPDATE player SET `16` = 9872 WHERE `id` = 276;
+UPDATE player SET `16` = 9871 WHERE `id` = 277;
+
+
+
+UPDATE player SET `16` = 9870 WHERE `id` = 289;
+
+
+
+
+
+
+
+
+
+
+UPDATE player SET `16` = 9869 WHERE `id` = 321;
+
+UPDATE player SET `16` = 9868 WHERE `id` = 324;
+UPDATE player SET `16` = 9867 WHERE `id` = 326;
+
+
+
+
+UPDATE player SET `16` = 9866 WHERE `id` = 335;
+
+
+
+
+
+
+
+
+
+UPDATE player SET `16` = 9865 WHERE `id` = 372;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+UPDATE player SET `16` = 9978 WHERE `id` = 23;
+
+
+
+
+
+UPDATE player SET `16` = 9977 WHERE `id` = 63;
+
+
+
+
+
+
+
+
+
+
+
+UPDATE player SET `16` = 9976 WHERE `id` = 182;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+UPDATE player SET `16` = 9975 WHERE `id` = 3;
+UPDATE player SET `16` = 9973 WHERE `id` = 7;
+UPDATE player SET `16` = 9974 WHERE `id` = 8;
+UPDATE player SET `16` = 9950 WHERE `id` = 14;
+UPDATE player SET `16` = 9949 WHERE `id` = 16;
+
+UPDATE player SET `16` = 9970 WHERE `id` = 22;
+UPDATE player SET `16` = 9965 WHERE `id` = 24;
+UPDATE player SET `16` = 9972 WHERE `id` = 32;
+UPDATE player SET `16` = 9964 WHERE `id` = 33;
+UPDATE player SET `16` = 9909 WHERE `id` = 35;
+UPDATE player SET `16` = 9910 WHERE `id` = 36;
+UPDATE player SET `16` = 9971 WHERE `id` = 37;
+UPDATE player SET `16` = 9956 WHERE `id` = 38;
+
+UPDATE player SET `16` = 9955 WHERE `id` = 42;
+UPDATE player SET `16` = 9969 WHERE `id` = 45;
+UPDATE player SET `16` = 9963 WHERE `id` = 46;
+UPDATE player SET `16` = 9961 WHERE `id` = 47;
+UPDATE player SET `16` = 9962 WHERE `id` = 48;
+UPDATE player SET `16` = 9957 WHERE `id` = 57;
+UPDATE player SET `16` = 9908 WHERE `id` = 61;
+UPDATE player SET `16` = 9907 WHERE `id` = 67;
+UPDATE player SET `16` = 9906 WHERE `id` = 68;
+UPDATE player SET `16` = 9960 WHERE `id` = 70;
+
+
+
+
+UPDATE player SET `16` = 9959 WHERE `id` = 78;
+UPDATE player SET `16` = 9905 WHERE `id` = 81;
+UPDATE player SET `16` = 9968 WHERE `id` = 83;
+
+UPDATE player SET `16` = 9958 WHERE `id` = 88;
+
+
+UPDATE player SET `16` = 9904 WHERE `id` = 91;
+
+
+
+UPDATE player SET `16` = 9901 WHERE `id` = 97;
+UPDATE player SET `16` = 9902 WHERE `id` = 101;
+UPDATE player SET `16` = 9903 WHERE `id` = 102;
+
+UPDATE player SET `16` = 9954 WHERE `id` = 108;
+
+UPDATE player SET `16` = 9953 WHERE `id` = 112;
+
+UPDATE player SET `16` = 9900 WHERE `id` = 114;
+UPDATE player SET `16` = 9952 WHERE `id` = 115;
+UPDATE player SET `16` = 9899 WHERE `id` = 116;
+
+UPDATE player SET `16` = 9967 WHERE `id` = 119;
+UPDATE player SET `16` = 9898 WHERE `id` = 120;
+
+
+UPDATE player SET `16` = 9897 WHERE `id` = 133;
+UPDATE player SET `16` = 9896 WHERE `id` = 135;
+UPDATE player SET `16` = 9951 WHERE `id` = 137;
+
+
+
+UPDATE player SET `16` = 9895 WHERE `id` = 144;
+
+UPDATE player SET `16` = 9894 WHERE `id` = 152;
+
+
+
+
+UPDATE player SET `16` = 9893 WHERE `id` = 164;
+
+UPDATE player SET `16` = 9948 WHERE `id` = 167;
+UPDATE player SET `16` = 9947 WHERE `id` = 173;
+UPDATE player SET `16` = 9966 WHERE `id` = 174;
+UPDATE player SET `16` = 9892 WHERE `id` = 175;
+
+
+
+
+UPDATE player SET `16` = 9891 WHERE `id` = 185;
+
+
+
+UPDATE player SET `16` = 9890 WHERE `id` = 203;
+
+UPDATE player SET `16` = 9889 WHERE `id` = 206;
+
+
+
+UPDATE player SET `16` = 9888 WHERE `id` = 211;
+
+
+
+
+
+UPDATE player SET `16` = 9887 WHERE `id` = 224;
+
+
+
+
+
+
+
+
+
+
+
+UPDATE player SET `16` = 9946 WHERE `id` = 269;
+
+
+
+
+
+
+
+
+
+UPDATE player SET `16` = 9945 WHERE `id` = 288;
+UPDATE player SET `16` = 9966 WHERE `id` = 293;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
